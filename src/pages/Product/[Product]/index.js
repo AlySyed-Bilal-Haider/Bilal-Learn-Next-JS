3f79bb697a779dd0bd7f09fded3071d29a921d00
AlySyed-Bilal-Haider <bilalshahbscs@gmail.com>
2023-03-03 15:32:17 +0500

    client navigations in Next.js

--- src/pages/Product/[Product]/index.js
+++ src/pages/Product/[Product]/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
 function index() {
-  return <div>index</div>;
+  return (
+    <>
+      {" "}
+      <h1>
+        <div>Products one</div>
+        <Link href="/Product/1">Product Ones</Link>
+      </h1>
+    </>
+  );
 }
 
 export default index;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>
+        <Link href="/Product">Product</Link>
+      </h1>
+      <h1>
+        <Link href="/Product/1">Product Ones</Link>
+      </h1>
+    </>
+  );
 }
 
 export default Home;
